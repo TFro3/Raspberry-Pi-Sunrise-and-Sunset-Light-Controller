406502e28d5dc2192c34cc16f32134e9774ddcf8
TFro3 <130847835+TFro3@users.noreply.github.com>
2023-11-26 11:53:16 -0600

    Add files via upload

--- LightController.py
+++ LightController.py
@@ -1,28 +1,18 @@
 import requests
 from gpiozero import OutputDevice
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import time
 
 # Configuration variables
-latitude = 'YOUR_LATITUDE'
-longitude = 'YOUR_LONGITUDE'
+latitude = '36.162663'
+longitude = '-86.781601'
 relay_pin = 14  # Replace with your GPIO pin number
-
-# Fallback sunrise and sunset times (in 12-hour clock format with AM/PM)
-fallback_sunrise_time = (7, 0, 'AM')  # 7:00 AM
-fallback_sunset_time = (7, 0, 'PM')   # 7:00 PM
 
 # Offset in minutes for sunrise and sunset
 sunrise_time_offset = 0
 sunset_time_offset = 0
 
-# Function to convert 12-hour time to 24-hour time
-def convert_to_24_hour(hour, minute, is_pm):
-    if is_pm:
-        return (hour + 12) % 24, minute
-    return hour, minute
-
-# Function to get today's sunrise and sunset times from API or fallback to default times
+# Function to get today's sunrise and sunset times from API
 def get_today_sunrise_sunset(latitude, longitude):
     try:
         url = f'https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}&formatted=0'
@@ -32,30 +22,17 @@ def get_today_sunrise_sunset(latitude, longitude):
         sunrise = datetime.strptime(data['results']['sunrise'], "%Y-%m-%dT%H:%M:%S%z")
         sunset = datetime.strptime(data['results']['sunset'], "%Y-%m-%dT%H:%M:%S%z")
 
-        print(f"Today's Sunrise: {sunrise.strftime('%Y-%m-%d %H:%M:%S')}")
-        print(f"Today's Sunset: {sunset.strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"Today's Sunrise: {sunrise.strftime('%Y-%m-%d %H:%M:%S %Z')}")
+        print(f"Today's Sunset: {sunset.strftime('%Y-%m-%d %H:%M:%S %Z')}")
 
-        return sunrise, sunset
+        return sunrise.replace(tzinfo=timezone.utc), sunset.replace(tzinfo=timezone.utc)
     except Exception as e:
         print(f"Error fetching sunrise/sunset times from API: {e}")
-        # Use default fallback times if API call fails
-        today = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
-
-        # Convert 12-hour format to 24-hour format for fallback times
-        default_sunrise_hour, default_sunrise_minute, is_pm_sunrise = fallback_sunrise_time
-        default_sunset_hour, default_sunset_minute, is_pm_sunset = fallback_sunset_time
-
-        default_sunrise = today.replace(*convert_to_24_hour(default_sunrise_hour % 12, default_sunrise_minute, is_pm_sunrise == 'PM'))
-        default_sunset = today.replace(*convert_to_24_hour(default_sunset_hour % 12, default_sunset_minute, is_pm_sunset == 'PM'))
-
-        print(f"Using Fallback Sunrise: {default_sunrise.strftime('%Y-%m-%d %H:%M:%S')}")
-        print(f"Using Fallback Sunset: {default_sunset.strftime('%Y-%m-%d %H:%M:%S')}")
-
-        return default_sunrise, default_sunset
+        return None, None
 
 # Function to calculate time until the next sunset and sunrise
 def time_until_next_sunrise_sunset(sunrise, sunset):
-    now = datetime.utcnow()
+    now = datetime.now(timezone.utc)
 
     next_sunrise = sunrise.replace(year=now.year, month=now.month, day=now.day)
     next_sunset = sunset.replace(year=now.year, month=now.month, day=now.day)
@@ -71,27 +48,26 @@ def time_until_next_sunrise_sunset(sunrise, sunset):
     return time_until_sunset, time_until_sunrise
 
 # GPIO Pin for the relay
-relay = OutputDevice(relay_pin)
+relay = OutputDevice(relay_pin, active_high=False, initial_value=False)
+
+# Fetch sunrise and sunset times for the day
+today_sunrise, today_sunset = get_today_sunrise_sunset(latitude, longitude)
 
 while True:
-    # Get today's sunrise and sunset times (fallback to defaults if API call fails)
-    today_sunrise, today_sunset = get_today_sunrise_sunset(latitude, longitude)
-
+    now = datetime.now(timezone.utc)
     time_until_sunset, time_until_sunrise = time_until_next_sunrise_sunset(today_sunrise, today_sunset)
 
-    if time_until_sunset < time_until_sunrise:
-        time.sleep(time_until_sunset + sunset_time_offset * 60)
-        print("Relay ON")
+    # Determine the state of the relay based on sunset and sunrise times
+    if time_until_sunset <= max(sunset_time_offset * 60, 0) and time_until_sunrise > 0:
+        print("Sunset Hours Reached: LIGHTS ON")
         relay.on()
-        time_until_sunset, time_until_sunrise = time_until_next_sunrise_sunset(today_sunrise, today_sunset)
-        time.sleep(time_until_sunset)
-        print("Relay OFF")
-        relay.off()
     else:
-        time.sleep(time_until_sunrise + sunrise_time_offset * 60)
-        print("Relay OFF")
+        print("Sunrise Hours Reached: LIGHTS OFF")
         relay.off()
-        time_until_sunset, time_until_sunrise = time_until_next_sunrise_sunset(today_sunrise, today_sunset)
-        time.sleep(time_until_sunrise)
-        print("Relay ON")
-        relay.on()
+
+    # Check if it's past midnight to fetch new sunrise and sunset times
+    if now.hour == 0 and now.minute == 0:
+        today_sunrise, today_sunset = get_today_sunrise_sunset(latitude, longitude)
+
+    # Sleep for a while before checking again
+    time.sleep(60)  # Sleep for 1 minute before rechecking
